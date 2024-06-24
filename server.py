7180041fe7c18f747518c94ebd517ee2a2171b3e
Magnat12 <rizamatmu@gmail.com>
2024-06-24 22:23:21 +0500

    beta version 0.2

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import sqlite3
 import threading
 import time
 import hashlib
+import secrets
 
 app = Flask(__name__)
 api = Api(app)
+token = ''
+list_token = ('1234567890')
+
+
 
 connect = sqlite3.connect('Chat.db', check_same_thread=False)
 cursor = connect.cursor()
@@ -24,6 +29,12 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS
                message TEXT,
                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)""")
 
+cursor.execute("""CREATE TABLE IF NOT EXISTS
+               TOK(
+               id TEXT PRIMARY KEY
+               )""")
+
+cursor.execute('PRAGMA journal_mode = OFF')
 connect.commit()
 
 class Name_gmail(Resource):
@@ -46,7 +57,7 @@ class Name_gmail(Resource):
             return {'message': 'Вы зарегистрированны'}
         else:
             return {'message': 'Вы уже зарегистрированны'}
-
+        
 class Send_message(Resource):
     def post(self):
         parser = reqparse.RequestParser()
@@ -70,18 +81,42 @@ class Get_messages(Resource):
         cursor.execute("SELECT id FROM users WHERE name = ?", (name,))
         user = cursor.fetchone()
         if not user:
-            return {'error': 'User does not exist'}, 400
+            return {'error': 'Пользователь не существует'}, 400
         cursor.execute("SELECT name, message FROM mes WHERE name = ?", (name,))
         messages = cursor.fetchall()
         if messages:
             message_list = [{'message': msg[1]} for msg in messages]
             return message_list
         else:
-            return {'message': 'No messages found'}, 200
+            return {'message': 'Сообщения не найдены'}, 200
+
+class Profil_user(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=str)
+        parser.add_argument('gmail', type=str)
+        parser.add_argument('password', type=str)
+        args = parser.parse_args()
+        name = args["name"]
+        gmail = args["gmail"]
+        password = args['password']
+        password_ha = hashlib.sha3_512(password.encode()).hexdigest()
+        cursor.execute("SELECT name, gmail, password FROM users WHERE name = ? AND gmail = ? AND password = ?", (name, gmail, password_ha))
+        data = cursor.fetchone()
+        if name in data:
+            token = ''.join(secrets.choice('0123456789') for _ in range(10))
+            cursor.execute('INSERT INTO TOK (id) VALUES (?)', (token,))
+            connect.commit()
+            return {'token': token}
+
+
+            
+
 
 api.add_resource(Name_gmail, "/api/regist")
 api.add_resource(Send_message, "/api/send_message")
 api.add_resource(Get_messages, "/api/get_messages/<string:name>")
+api.add_resource(Profil_user, "/api/user")
 
 
 def delete_old_messages():
