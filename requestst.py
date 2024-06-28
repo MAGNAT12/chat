40960d7a9df82cfbfe2ba4c4eb721eaac0c3a92f
Magnat12 <rizamatmu@gmail.com>
2024-06-28 16:31:51 +0500

    version 0.1

--- requestst.py
+++ requestst.py
@@ -1,30 +1,49 @@
 import json
 import requests
 import emoji
+import platform
 
 headers = {'Content-Type': 'application/json'}
+name_devices = platform.node()
 
 def regist():
     date = {
         "name":"Magnat",
         "gmail":"rizamatmu@gmail.com",
-        "password":"123456789"
+        "password":"123456789",
     }
 
     respons = requests.post("http://127.0.0.1:3000/api/regist", data=json.dumps(date), headers=headers)
     print(respons.json())
 
+
 def message():
     date = {
         "name":"Magnat",
-        "message":"привет"
+        "message":"привет",
+        "name_devices": name_devices
     }
     respons = requests.post("http://127.0.0.1:3000/api/send_message", data=json.dumps(date), headers=headers)
     print(respons.json())
 
 def get():
-    name = "Magnat"
-    respons = requests.get(f"http://127.0.0.1:3000/api/get_messages/{name}")
+    date = {
+        "name_devices":name_devices
+    }
+    respons = requests.get(f"http://127.0.0.1:3000/api/get_messages", data = json.dumps(date), headers=headers)
     print(respons.json())
 
-regist()
+
+def profil():
+    date = {
+        "name":"Magnat",
+        "gmail":"rizamatmu@gmail.com",
+        "password":"123456789",
+        "name_devices": name_devices
+    }
+
+    respons = requests.post("http://127.0.0.1:3000/api/user", data=json.dumps(date), headers=headers)
+    print(respons.json())
+
+get()
+
--- server.py
+++ server.py
@@ -1,16 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
 import sqlite3
 import threading
 import time
 import hashlib
-import secrets
+
 
 app = Flask(__name__)
 api = Api(app)
 token = ''
 list_token = ('1234567890')
-
 
 
 connect = sqlite3.connect('Chat.db', check_same_thread=False)
@@ -25,13 +24,15 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS
                mes(
+               name_sender TEXT,
                name TEXT,
                message TEXT,
                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS
-               TOK(
-               id TEXT PRIMARY KEY
+               devices(
+               name TEXT,
+               name_devices TEXT
                )""")
 
 cursor.execute('PRAGMA journal_mode = OFF')
@@ -63,59 +64,80 @@ class Send_message(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument("name", type=str)
         parser.add_argument("message", type=str)
+        parser.add_argument("name_devices", type=str)
         args = parser.parse_args()
         name = args["name"]
+        name_devices = args['name_devices']
         cursor.execute(f"SELECT `name` FROM `users` WHERE `name` = '{name}'")
         data = cursor.fetchone()
         if data:
-            name = args["name"]
-            message = args["message"]
-            cursor.execute("INSERT INTO mes(name, message, timestamp) VALUES(?, ?, datetime('now'));", (name, message))
-            connect.commit()
-            return {'message': 'сообщение отправлено'}
+            cursor.execute(f"SELECT `name_devices` FROM devices WHERE `name_devices` = '{name_devices}'")
+            data = cursor.fetchone()
+            if data:
+                name = args["name"]
+                message = args["message"]
+                cursor.execute('SELECT name FROM devices WHERE `name_devices` = ?', (name_devices,))
+                name_sender = cursor.fetchone()
+                name_sender = name_sender[0]
+                cursor.execute(
+                "INSERT INTO mes(name_sender, name, message, timestamp) VALUES(?, ?, ?, datetime('now'));",
+                (name_sender, name, message)
+                            )
+                connect.commit()
+                connect.close()
+                return {'message': 'сообщение отправлено'}
+            else:
+                return {"message":"Вы не вошли в профель"}
         else:
-            return {'message': "сообщение не отправлено"}
+            return {'message': "НЕ найде пользователь"}
 
 class Get_messages(Resource):
-    def get(self, name):
-        cursor.execute("SELECT id FROM users WHERE name = ?", (name,))
-        user = cursor.fetchone()
-        if not user:
-            return {'error': 'Пользователь не существует'}, 400
-        cursor.execute("SELECT name, message FROM mes WHERE name = ?", (name,))
-        messages = cursor.fetchall()
-        if messages:
-            message_list = [{'message': msg[1]} for msg in messages]
-            return message_list
-        else:
-            return {'message': 'Сообщения не найдены'}, 200
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name_devices", type=str)
+        args = parser.parse_args()
+        name_devices = args['name_devices']
+        cursor.execute(f"SELECT `name_devices` FROM devices WHERE `name_devices` = '{name_devices}'")
+        data = cursor.fetchone()
+        if data:
+            cursor.execute('SELECT name FROM devices WHERE `name_devices` = ?', (name_devices,))
+            name_sender = cursor.fetchone()
+            name_sender = name_sender[0]
+            cursor.execute("SELECT name_sender, message, timestamp FROM mes WHERE name = ?", (name_sender,))
+            messages = cursor.fetchall()
+            if messages:
+                messages_list = [{'name_sender': msg[0], 'message': msg[1]} for msg in messages]
+                return jsonify({'messages': messages_list})
+            else:
+                return {'message': 'Сообщений не найдено'}
+                
 
+        
 class Profil_user(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument("name", type=str)
         parser.add_argument('gmail', type=str)
         parser.add_argument('password', type=str)
+        parser.add_argument("name_devices", type=str)
         args = parser.parse_args()
         name = args["name"]
         gmail = args["gmail"]
         password = args['password']
+        name_devices = args['name_devices']
         password_ha = hashlib.sha3_512(password.encode()).hexdigest()
         cursor.execute("SELECT name, gmail, password FROM users WHERE name = ? AND gmail = ? AND password = ?", (name, gmail, password_ha))
         data = cursor.fetchone()
         if name in data:
-            token = ''.join(secrets.choice('0123456789') for _ in range(10))
-            cursor.execute('INSERT INTO TOK (id) VALUES (?)', (token,))
+            cursor.execute('INSERT INTO devices (name_devices, name) VALUES (?, ?)', (name_devices, name))
             connect.commit()
-            return {'token': token}
-
-
-            
+        else:
+            return {"message":"Зарегистрируйтесь"}
 
 
 api.add_resource(Name_gmail, "/api/regist")
 api.add_resource(Send_message, "/api/send_message")
-api.add_resource(Get_messages, "/api/get_messages/<string:name>")
+api.add_resource(Get_messages, "/api/get_messages")
 api.add_resource(Profil_user, "/api/user")
 
 
